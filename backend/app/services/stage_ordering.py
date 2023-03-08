256b1e0eeb714adc720666af58f80b9f26862b10
garyson26 <developer@wiestell.com>
2023-03-08 17:27:55 +0530

    feat(stage_ordering): surface the first cycle in the dependency graph

--- backend/app/services/stage_ordering.py
+++ backend/app/services/stage_ordering.py
@@ -17,3 +17,29 @@ def sort_deps(graph):
     for node in sorted(graph):
         visit(node)
     return done
+
+
+def first_cycle(graph):
+    """Return one cycle as a node list, or None if the graph is acyclic."""
+    path, on_path = [], set()
+
+    def walk(node):
+        if node in on_path:
+            return path[path.index(node):] + [node]
+        if node not in graph:
+            return None
+        path.append(node)
+        on_path.add(node)
+        for dep in graph.get(node, ()):
+            found = walk(dep)
+            if found:
+                return found
+        path.pop()
+        on_path.discard(node)
+        return None
+
+    for node in sorted(graph):
+        found = walk(node)
+        if found:
+            return found
+    return None
