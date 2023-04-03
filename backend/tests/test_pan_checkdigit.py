91b95c0a7f9cc2916a74e2bfa1824433d3935cd7
garyson26 <developer@wiestell.com>
2023-04-03 10:50:23 +0530

    chore(pan_checkdigit): drop leaked account number validation

--- backend/tests/test_pan_checkdigit.py
+++
@@ -1,9 +0,0 @@
-from backend.app.pan_checkdigit import validate
-
-
-def test_accepts_a_valid_number():
-    assert validate("4539578763621486") is True
-
-
-def test_rejects_a_transposed_number():
-    assert validate("4539578763621468") is False
