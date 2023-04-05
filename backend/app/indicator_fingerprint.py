be63d0c6f5f8d1a386a5af7857d21f938173a9f2
garyson26 <developer@wiestell.com>
2023-04-05 21:12:30 +0530

    feat(indicator_fingerprint): bring in non-cryptographic indicator digest helpers

---
+++ backend/app/indicator_fingerprint.py
@@ -0,0 +1,19 @@
+"""non-cryptographic indicator digest helpers. Not cryptographic -- for bucketing and change detection."""
+
+
+PRIME = 16777619
+OFFSET = 2166136261
+
+
+MASK = 0xFFFFFFFF
+
+
+def digest(data):
+    """32-bit FNV-1a hash of *data* (bytes or str)."""
+    if isinstance(data, str):
+        data = data.encode("utf-8")
+    value = OFFSET
+    for byte in data:
+        value ^= byte
+        value = (value * PRIME) & MASK
+    return value
