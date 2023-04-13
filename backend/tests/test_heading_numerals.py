74b32dd7c2fd868f3d37c76788d96116b28794ad
garyson26 <developer@wiestell.com>
2023-04-13 18:14:51 +0530

    chore(heading_numerals): drop roman numeral conversion for report headings

--- backend/tests/test_heading_numerals.py
+++
@@ -1,9 +0,0 @@
-from backend.app.schemas.heading_numerals import decode, encode
-
-
-def test_encodes_subtractive_forms():
-    assert encode(1994) == "MCMXCIV"
-
-
-def test_round_trips():
-    assert decode(encode(2024)) == 2024
