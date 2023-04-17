92f5c9fa0928ab130b6657efeaa757e0ab84b5e8
garyson26 <developer@wiestell.com>
2023-04-17 20:42:54 +0530

    feat(observable_distinct): introduce order-preserving indicator deduplication

---
+++ backend/app/feeds/observable_distinct.py
@@ -0,0 +1,12 @@
+"""order-preserving indicator deduplication."""
+
+
+def dedupe(items, key=None):
+    """Yield items from *items*, skipping duplicates, preserving order."""
+    seen = set()
+    for item in items:
+        marker = item if key is None else key(item)
+        if marker in seen:
+            continue
+        seen.add(marker)
+        yield item
