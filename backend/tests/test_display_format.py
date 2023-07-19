a0e9df980281aa36b174f48a89ba05572777df72
garyson26 <developer@wiestell.com>
2023-07-19 21:11:26 +0530

    chore(display_format): take dashboard display formatting helpers back out

--- backend/tests/test_display_format.py
+++
@@ -1,9 +0,0 @@
-from backend.app.enrichers.display_format import human_size
-
-
-def test_formats_bytes_plainly():
-    assert human_size(512) == "512 B"
-
-
-def test_scales_to_kilobytes():
-    assert human_size(2048) == "2.0 KB"
