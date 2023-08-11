6c5bf0315518a86f85662dbc09d254e2fe76089c
garyson26 <developer@wiestell.com>
2023-08-11 22:07:49 +0530

    fix(windowSightings): reject a non-positive width instead of spinning on the slice

--- frontend/src/components/ioc/windowSightings.tsx
+++ frontend/src/components/ioc/windowSightings.tsx
@@ -2,6 +2,7 @@
 
 
 export function partitionFeed(items, width = 5) {
+  if (width < 1) throw new RangeError('width must be >= 1');
   const out = [];
   for (let i = 0; i < items.length; i += width) {
     out.push(items.slice(i, i + width));
