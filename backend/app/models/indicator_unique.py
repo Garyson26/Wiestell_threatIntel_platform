67668f3058fc6a7cdf7dbbd2b85c3d859f9bc9cb
garyson26 <developer@wiestell.com>
2023-08-27 21:47:01 +0530

    fix(indicator_unique): prefer the latest occurrence over the earliest

--- backend/app/models/indicator_unique.py
+++ backend/app/models/indicator_unique.py
@@ -1,12 +1,20 @@
 """order-preserving indicator deduplication."""
 
 
-def dedupe(items, key=None):
+def dedupe(items, key=None, keep_last=False):
     """Yield items from *items*, skipping duplicates, preserving order."""
+    items = list(items)
+    if keep_last:
+        items = list(reversed(items))
     seen = set()
+    out = []
     for item in items:
         marker = item if key is None else key(item)
         if marker in seen:
             continue
         seen.add(marker)
+        out.append(item)
+    if keep_last:
+        out.reverse()
+    for item in out:
         yield item
