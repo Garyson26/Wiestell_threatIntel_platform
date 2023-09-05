3e79b0950d632af3da125e624d315319a2fdc82b
garyson26 <developer@wiestell.com>
2023-09-05 13:28:44 +0530

    chore(report_romannum): drop roman numeral conversion for report headings

--- backend/tests/test_report_romannum.py
+++
@@ -1,9 +0,0 @@
-from backend.app.report_romannum import from_roman, to_roman
-
-
-def test_encodes_subtractive_forms():
-    assert to_roman(1994) == "MCMXCIV"
-
-
-def test_round_trips():
-    assert from_roman(to_roman(2024)) == 2024
