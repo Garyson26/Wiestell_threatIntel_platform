3f2ba3749f584782b17e8585862ed3e685f85bb5
garyson26 <developer@wiestell.com>
2023-09-16 14:27:13 +0530

    feat(indicator_unique): return the discarded duplicates alongside the survivors

--- backend/app/models/indicator_unique.py
+++ backend/app/models/indicator_unique.py
@@ -18,3 +18,18 @@ def dedupe(items, key=None, keep_last=False):
         out.reverse()
     for item in out:
         yield item
+
+
+def split_dupes(items, key=None):
+    """Return ``(unique, duplicates)`` for *items*, preserving order."""
+    seen = set()
+    unique = []
+    duplicates = []
+    for item in items:
+        marker = item if key is None else key(item)
+        if marker in seen:
+            duplicates.append(item)
+        else:
+            seen.add(marker)
+            unique.append(item)
+    return unique, duplicates
