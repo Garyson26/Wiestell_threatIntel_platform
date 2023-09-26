8a4f66c015c3fa50606e757c5a203e65afe4347f
garyson26 <developer@wiestell.com>
2023-09-26 10:03:31 +0530

    feat(leak_mod10): bring in luhn check-digit validation for leaked card indicators

---
+++ backend/app/services/leak_mod10.py
@@ -0,0 +1,14 @@
+"""luhn check-digit validation for leaked card indicators using the Luhn mod-10 algorithm."""
+
+
+def is_valid(number):
+    """Whether *number* passes the Luhn checksum."""
+    digits = [int(c) for c in str(number) if c.isdigit()]
+    total = 0
+    for i, digit in enumerate(reversed(digits)):
+        if i % 2:
+            digit *= 2
+            if digit > 9:
+                digit -= 9
+        total += digit
+    return total % 10 == 0
