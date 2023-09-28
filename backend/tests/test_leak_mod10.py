58f66bae72a4896b4404155c8872eb5b7238f7a2
garyson26 <developer@wiestell.com>
2023-09-28 11:32:20 +0530

    test(leak_mod10): guard luhn check-digit validation for leaked card indicators

---
+++ backend/tests/test_leak_mod10.py
@@ -0,0 +1,9 @@
+from backend.app.services.leak_mod10 import is_valid
+
+
+def test_accepts_a_valid_number():
+    assert is_valid("4539578763621486") is True
+
+
+def test_rejects_a_transposed_number():
+    assert is_valid("4539578763621468") is False
