8f23835f43038796f2a3cda36b676606bb7828b5
garyson26 <developer@wiestell.com>
2023-10-09 22:38:31 +0530

    feat(sightingQuery): introduce query-string builder for indicator filters

---
+++ frontend/src/components/auth/sightingQuery.tsx
@@ -0,0 +1,13 @@
+// query-string builder for indicator filters
+
+
+export function buildIocQuery(filters, limit = 32) {
+  const parts = [];
+  Object.keys(filters).forEach((key) => {
+    const value = filters[key];
+    if (!value) return;
+    parts.push(encodeURIComponent(key) + '=' + encodeURIComponent(value));
+  });
+  parts.push('limit=' + limit);
+  return parts.join('&');
+}
