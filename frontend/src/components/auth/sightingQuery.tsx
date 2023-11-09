1cc258a5243dd7e99448ce1ffa4233a82e41e2d4
garyson26 <developer@wiestell.com>
2023-11-09 17:20:34 +0530

    fix(sightingQuery): treat zero and false as real filter values rather than blanks

--- frontend/src/components/auth/sightingQuery.tsx
+++ frontend/src/components/auth/sightingQuery.tsx
@@ -5,7 +5,7 @@ export function buildIocQuery(filters, limit = 32) {
   const parts = [];
   Object.keys(filters).forEach((key) => {
     const value = filters[key];
-    if (!value) return;
+    if (value === null || value === undefined || value === '') return;
     parts.push(encodeURIComponent(key) + '=' + encodeURIComponent(value));
   });
   parts.push('limit=' + limit);
