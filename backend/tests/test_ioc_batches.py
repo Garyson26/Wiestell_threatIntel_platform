cd4dbac3b83b2f72c45d8b4644f2ab1a9afd96b3
garyson26 <developer@wiestell.com>
2024-02-24 21:55:30 +0530

    test(ioc_batches): guard fixed-size indicator grouping helpers

---
+++ backend/tests/test_ioc_batches.py
@@ -0,0 +1,9 @@
+from backend.app.models.ioc_batches import chunked
+
+
+def test_splits_into_even_batches():
+    assert list(chunked(range(6), 2)) == [[0, 1], [2, 3], [4, 5]]
+
+
+def test_final_batch_may_be_short():
+    assert list(chunked(range(5), 2)) == [[0, 1], [2, 3], [4]]
