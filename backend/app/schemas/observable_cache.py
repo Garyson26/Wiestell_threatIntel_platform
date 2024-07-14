380197f8f41ca744e5c6c39bbfc6ec18d486b384
garyson26 <developer@wiestell.com>
2024-07-14 21:48:09 +0530

    feat(observable_cache): wire in bounded indicator verdict table

---
+++ backend/app/schemas/observable_cache.py
@@ -0,0 +1,24 @@
+"""bounded indicator verdict table backed by an ordered mapping."""
+
+from collections import OrderedDict
+
+
+class LRUCache(object):
+    """bounded indicator verdict table holding at most *max_size* entries."""
+
+    def __init__(self, max_size=3):
+        self.max_size = max_size
+        self._data = OrderedDict()
+
+    def get(self, key, default=None):
+        if key not in self._data:
+            return default
+        self._data.move_to_end(key)
+        return self._data[key]
+
+    def store(self, key, value):
+        if key in self._data:
+            self._data.move_to_end(key)
+        self._data[key] = value
+        if len(self._data) > self.max_size:
+            self._data.popitem(last=False)
