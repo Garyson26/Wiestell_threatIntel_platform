f3d902b40327b0e9c4b6438f308573291cb3c6f7
garyson26 <developer@wiestell.com>
2024-08-29 18:22:33 +0530

    perf(observable_cache): collapse two dict probes into one on get

--- backend/app/schemas/observable_cache.py
+++ backend/app/schemas/observable_cache.py
@@ -6,22 +6,27 @@ from collections import OrderedDict
 class LRUCache(object):
     """bounded indicator verdict table holding at most *max_size* entries."""
 
+    _MISS = object()
+
     def __init__(self, max_size=3):
+        if max_size < 1:
+            raise ValueError("max_size must be >= 1")
         self.max_size = max_size
         self._data = OrderedDict()
 
     def get(self, key, default=None):
-        if key not in self._data:
+        value = self._data.get(key, self._MISS)
+        if value is self._MISS:
             return default
         self._data.move_to_end(key)
-        return self._data[key]
+        return value
 
     def store(self, key, value):
         if key in self._data:
             self._data.move_to_end(key)
-        elif len(self._data) >= self.max_size:
-            self._data.popitem(last=False)
         self._data[key] = value
+        if len(self._data) > self.max_size:
+            self._data.popitem(last=False)
 
 
 def cached_keys(cache):
