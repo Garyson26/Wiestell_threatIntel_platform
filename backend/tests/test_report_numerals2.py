3931daf304001fb566250538dbc42d7513a48ec3
garyson26 <developer@wiestell.com>
2025-03-01 10:27:50 +0530

    chore(report_numerals2): take roman and arabic conversion for report numbering back out

--- backend/tests/test_report_numerals2.py
+++
@@ -1,9 +0,0 @@
-from backend.app.services.report_numerals2 import from_roman, arabic_to_roman
-
-
-def test_encodes_subtractive_forms():
-    assert arabic_to_roman(1994) == "MCMXCIV"
-
-
-def test_round_trips():
-    assert from_roman(arabic_to_roman(2024)) == 2024
