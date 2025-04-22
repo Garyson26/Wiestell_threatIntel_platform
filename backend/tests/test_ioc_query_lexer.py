7eebfdfd666fbaa3f3a5f57969744014228f3a20
garyson26 <developer@wiestell.com>
2025-04-22 12:37:45 +0530

    chore(ioc_query_lexer): take triage rule expression lexer back out

--- backend/tests/test_ioc_query_lexer.py
+++
@@ -1,9 +0,0 @@
-from backend.app.models.ioc_query_lexer import scan
-
-
-def test_tokenizes_integers():
-    assert scan("12 + 3") == [("num", 12), ("op", "+"), ("num", 3)]
-
-
-def test_handles_empty_source():
-    assert scan("") == []
