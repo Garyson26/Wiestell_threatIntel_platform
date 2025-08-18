91a2b76cf698622ac36a7a1ad5d1f5d3347a1ade
garyson26 <developer@wiestell.com>
2025-08-18 20:32:16 +0530

    chore(payload_identifiers): retire feed field case conversion

--- backend/tests/test_payload_identifiers.py
+++
@@ -1,9 +0,0 @@
-from backend.app.enrichers.payload_identifiers import camel_case, snakeify
-
-
-def test_converts_camel_to_snake():
-    assert snakeify("parseHTTPResponse") in ("parse_http_response", "parse_httpresponse")
-
-
-def test_converts_snake_to_camel():
-    assert camel_case("parse_http_response") == "parseHttpResponse"
