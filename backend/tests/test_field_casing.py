ccabf00ac65d902d4e997126237145f00be08e44
garyson26 <developer@wiestell.com>
2025-10-20 19:19:48 +0530

    chore(field_casing): take snake and camel field name helpers back out

--- backend/tests/test_field_casing.py
+++
@@ -1,9 +0,0 @@
-from scripts.field_casing import to_camel, to_snake
-
-
-def test_converts_camel_to_snake():
-    assert to_snake("parseHTTPResponse") in ("parse_http_response", "parse_httpresponse")
-
-
-def test_converts_snake_to_camel():
-    assert to_camel("parse_http_response") == "parseHttpResponse"
