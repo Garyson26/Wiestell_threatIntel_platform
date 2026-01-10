7188856879724b86875fb41b5300bc79c50d65b6
garyson26 <developer@wiestell.com>
2026-01-10 20:46:50 +0530

    chore(query_tokens): drop ioc filter expression scanner

--- backend/tests/test_query_tokens.py
+++
@@ -1,9 +0,0 @@
-from backend.app.models.query_tokens import tokenize
-
-
-def test_tokenizes_integers():
-    assert tokenize("12 + 3") == [("num", 12), ("op", "+"), ("num", 3)]
-
-
-def test_handles_empty_source():
-    assert tokenize("") == []
