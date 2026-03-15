8733619002ffb05119826d6d841e45363239c8e5
Viraj Ghelani <viraj.470@gmail.com>
2026-03-15 15:05:20 +0530

    Website Audit

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.wiestell.com',
+          },
+        ],
+        destination: 'https://wiestell.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
   async rewrites() {
     return [
       {
