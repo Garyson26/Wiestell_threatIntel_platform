65eea661454c1940127777f14154c5bfcd404fc2
Viraj Ghelani <viraj.470@gmail.com>
2026-03-21 22:50:57 +0530

    tag update

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -96,6 +96,21 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Google tag (gtag.js) */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-FG3RH76R6J"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-FG3RH76R6J');
+            `,
+          }}
+        />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
